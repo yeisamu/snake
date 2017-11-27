07a7517df40cb5b755ad42c8795dce3bcbffa59e
SAMUEL\Yeison <yeison.velasquez@gmail.com>
2017-11-27 12:22:08 -0500

    Trabajo final

---
+++ snake3/Enemigo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake3
+{
+    class Enemigo : objeto
+    {
+        public Enemigo()
+        {
+            this.x = generar(102);
+            this.y = generar(50);
+        }
+        public void dibujar(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Black), this.x, this.y, 30, 50);
+        }
+        public void colocar()
+        {
+            this.x = generar(102);
+            this.y = generar(50);
+        }
+        public int generar(int n)
+        {
+            Random random = new Random();
+            int num = random.Next(0, n) * 10;
+            return num;
+        }
+        public int verXobs()
+        {
+            return this.x;
+        }
+        public int verYobs()
+        {
+            return this.y;
+        }
+    }
+}
